c87a9d240726eb40caf607d7bfa01cad64ebfc89
Florin Cioroianu <florincioroianu23@gmail.com>
2022-07-08 15:01:27 +0300

    CategoryController finish

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -18,8 +18,10 @@ class CategoryController extends ApiController
      */
     public function getAll(Request $request)
     {
-
+        $categories=Category::all();
+        return $this->sendResponse($categories->toArray());
     }
+
 
     /**
      * @param Request $request
@@ -64,25 +66,62 @@ class CategoryController extends ApiController
     /**
      * @param $id
      */
-    public function get($id)
+    public function get(Request $request)
     {
-
+        $id=$request->get('id');
+        $category=Category::find($id);
+        return $this->sendResponse($category->toArray());
     }
 
     /**
      * @param $id
      * @param Request $request
      */
-    public function update($id, Request $request)
+    public function update(Request $request)
     {
+        try {
+            $validator = Validator::make($request->all(), [
+                'name' => 'nullable|max:50',
+                'parent_id' => 'nullable|exists:categories,id'
+            ]);
 
+            if ($validator->fails()) {
+                return $this->sendError('Bad request!', $validator->messages()->toArray());
+            }
+
+            $id=$request->get('id');
+            $name = $request->get('name');
+            $parentId = $request->get('parent_id');
+
+            if ($parentId) {
+                $parent = Category::find($parentId);
+
+                if ($parent->parent?->parent) {
+                    return $this->sendError('You can\'t add a 3rd level subcategory!');
+                }
+            }
+
+            $category = Category::find($id);
+            $category->name = $name;
+            $category->parent_id = $parentId;
+            $category->save();
+
+            return $this->sendResponse($category->toArray());
+        } catch (\Exception $exception) {
+            Log::error($exception);
+
+            return $this->sendError('Something went wrong, please contact administrator!');
+        }
     }
 
     /**
      * @param $id
      */
-    public function delete($id)
+    public function delete(Request $request)
     {
-
+        $id=$request->get('id');
+        $category=Category::find($id);
+        $category->delete();
+        return $this->sendResponse($category->delete(), 'Category with id '.$id. ' deleted');
     }
 }
